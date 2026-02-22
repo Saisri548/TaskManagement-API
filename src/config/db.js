c89196ef6e9a5ee06b89f71b5cdad92ba80838e0
Saisri <thotasaisri7@gmail.com>
2026-02-22 13:33:43 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,9 @@
-import pkg from "pg"
-const {Pool}=pkg;
-import dotenv from "dotenv"
-dotenv.config();
-const pool=new Pool({
-    host:process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    name:process.env.DB_NAME
-})
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // needed for Railway
+});
+
 export default pool;
